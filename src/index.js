b53e63e7f5717bd568e5a61c56f5fd6b8091daf5
SEAF39 <labber@DESKTOP-7J1C764>
2023-05-25 01:12:07 -0400

    reafactoring the code

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const Button = (props) => {
+  return <button onClick={props.onClick}>{props.children}</button>;
+};
+
+const Application = () => {
+  const [name, setName] = useState("");
+
+  const reset = () => {
+    console.log("reset");
+    setName("");
+  };
+
+  return (
+    <main>
+      <input
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        placeholder="Type your name"
+      />
+      <Button onClick={reset}>Reset</Button>
+      {name && <h1>Hello {name}</h1>}
+    </main>
+  );
+};
+
+ReactDOM.render(<Application />, document.getElementById("root"));
